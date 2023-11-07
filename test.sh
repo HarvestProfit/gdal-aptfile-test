6f8fb44345c8324eebf0f047ae6c3cb6dc286d40
Codzter <32208000+cody-elhard@users.noreply.github.com>
2023-11-07 10:34:27 -0600

    bash script

--- test.sh
+++ test.sh
@@ -1,1 +1,2 @@
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/tes
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/app/.apt/usr/lib/x86_64-linux-gnu/blas
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/app/.apt/usr/lib/x86_64-linux-gnu/lapack
